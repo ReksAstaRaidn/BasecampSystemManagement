cfb7a1376ef25a74d215c3981d2697b32f39bd19
ReksAsta <fnoor6612@gmail.com>
2026-05-21 17:26:04 +0700

    buat method baru dimana key tidak bisa di duplikasi

--- HashDataPendaki.py
+++ HashDataPendaki.py
@@ -9,16 +9,21 @@ class Hash:
         #
         return numeric_part % self.size
     
+    def cek_duplikat_id(self, idTicket):
+        index = self._hash(idTicket)
+        for data in self.table[index]:
+            if data["id"] == idTicket:
+                return True
+        return False
+
     def daftar_pendaki(self, idTicket, nama, kontak):
         index = self._hash(idTicket)
-        dataPendaki = {"id": idTicket, "nama": nama,"kontak": kontak}
-        #cek apakah idTicket sudah ada, jika tidak ada tambahkan data baru
-        for i, data in enumerate(self.table[index]):
-            if data["id"] == idTicket:
-                self.table[index][i] = dataPendaki
-                return
-            
+        if self.cek_duplikat_id(idTicket):
+            return False
+
+        dataPendaki = {"id": idTicket, "nama": nama, "kontak": kontak}
         self.table[index].append(dataPendaki)
+        return True
         
     def verifikasi_pendaki(self, idTicket):
         index = self._hash(idTicket)
@@ -28,3 +33,10 @@ class Hash:
                 return data
         return None
         
+    def cek_seluruh_id(self):
+        #ambil semua idTicket yang terdaftar
+        id_list = []
+        for bucket in self.table:
+            for data in bucket:
+                id_list.append(data["id"])
+        return id_list
