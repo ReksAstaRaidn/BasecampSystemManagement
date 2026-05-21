61dbd7c0f84efbc5beea3e695605f04e73859bee
ReksAsta <fnoor6612@gmail.com>
2026-05-22 01:38:22 +0700

    add comment

--- HashDataPendaki.py
+++ HashDataPendaki.py
@@ -1,14 +1,14 @@
 class Hash:
-    def __init__(self, size=100):
+    def __init__(self, size=1000):
         self.size = size
         self.table = [[] for _ in range(size)]
-        
+    
+    # fungsi hash sederhana yang mengambil bagian numerik dari idTicket dan menggunakan modulus untuk menentukan index
     def _hash(self, idTicket):
-        #ambil angka dari idTicket
-        numeric_part = int (''.join(filter(str.isdigit, idTicket)))
-        #
+        numeric_part = int (''.join(filter(str.isdigit, idTicket))) 
         return numeric_part % self.size
     
+    #cek apakah idTicket sudah terdaftar untuk mencegah duplikasi
     def cek_duplikat_id(self, idTicket):
         index = self._hash(idTicket)
         for data in self.table[index]:
@@ -16,15 +16,16 @@ class Hash:
                 return True
         return False
 
+    #daftarkan pendaki dengan idTicket, nama, dan kontak jika ID belum ada
     def daftar_pendaki(self, idTicket, nama, kontak):
         index = self._hash(idTicket)
         if self.cek_duplikat_id(idTicket):
             return False
-
         dataPendaki = {"id": idTicket, "nama": nama, "kontak": kontak}
         self.table[index].append(dataPendaki)
         return True
-        
+    
+    #verifikasi pendaki berdasarkan idTicket untuk memastikan data yang benar saat antri
     def verifikasi_pendaki(self, idTicket):
         index = self._hash(idTicket)
         #cari data dengan idTicket yang sesuai
@@ -32,7 +33,7 @@ class Hash:
             if data["id"] == idTicket:
                 return data
         return None
-        
+    
     def cek_seluruh_id(self):
         #ambil semua idTicket yang terdaftar
         id_list = []
